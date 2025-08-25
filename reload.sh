3aecb7345f514486039b5b8237e2c07c1539df22
Storcale <105071746+storcale@users.noreply.github.com>
2025-08-25 14:28:52 +0200

    reload

--- reload.sh
+++ reload.sh
@@ -2,9 +2,5 @@ echo "[Deploy]"
 git pull
 bun install
 echo "[RELOAD] Restarting API"
-screen -S api -X quit || true
-screen -dmS api bash -c "
-  cd storcale-api
-  exec node app.js
-"  
+exec node app.js
 echo "[RELOAD] Done"
