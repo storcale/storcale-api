36e14bf44d7afaa9ac21c0ef8177b6a811d69d57
storcale <storcale.dev@gmail.com>
2026-07-14 22:14:28 +0900

    Fix deploy.sh : Chapter 2

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 set -e
 
 cd "$(dirname "$0")"
-bun install pm2 -g
-export PATH="/home/storcale/.bun/bin:$PATH"
 git pull
 bun install
 bun run test
 pm2 restart ecosystem.config.js --update-env
+./node_modules/.bin/pm2 restart ecosystem.config.js --update-env
